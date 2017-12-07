0d55a127c8b23869439c2da45a8d2afc2ae9f4c6
samrita <sam@capiot.com>
2017-12-07 14:30:54 +0530

    Initial Release

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
+'use strict';
+
 var initialize  = require('./initialize');
 var crawler = require('./crawler');
 
 
-// -------------------Initialize by asking the questionnaire-----------------
-initialize.init();
-
-// -------------------User choices captured---------------------------------- 
-initialize.startQuestionnaire().then(arguements => {
-    
-    var startUrl = arguements['0']['startUrl'];
-    var searchWord = arguements['0']['searchWord'];
-    var maxPagesToVisit = parseInt(arguements['0']['maxPagesToVisit']);
-    
-    //-------------------The Crawler starts to crawl-----------------------------
-    crawler.init(startUrl,searchWord,maxPagesToVisit);
-    crawler.startCrawling();
-});
+module.exports = {
+    init: initialize.init(),
+    // -------------------User choices captured---------------------------------- 
+    startCrawling : initialize.startQuestionnaire().then(arguements => {
+        
+        var startUrl = arguements['0']['startUrl'];
+        var searchWord = arguements['0']['searchWord'];
+        var maxPagesToVisit = parseInt(arguements['0']['maxPagesToVisit']);
+        
+        //-------------------The Crawler starts to crawl-----------------------------
+        crawler.init(startUrl,searchWord,maxPagesToVisit);
+        crawler.startCrawling();
+    })
+}
 
 
 
